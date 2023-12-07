2afc8fb9b531fef2d71276f3464c9dcdf3eda4ae
Simon Zhong <simon.zhong@sentry.io>
2023-12-06 17:23:27 -0800

    add logic in before_send to return nil if tag handled is true

--- lib/helpers/sentry.ex
+++ lib/helpers/sentry.ex
@@ -1,6 +1,11 @@
 defmodule Helpers.Sentry do
   def before_send(event) do
     tags = Map.merge(event.tags, %{se: "tda"})
-    %Sentry.Event{event | tags: tags}
+    if Enum.any?(tags, fn {_, handled} -> handled == true end) do
+      IO.puts("handled is true, so the exception isn't sent to Sentry.")
+      nil
+    else
+      %Sentry.Event{event | tags: tags}
+    end
   end
 end
